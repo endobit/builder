48667d33710abb08a33a3a68f76c251013e3d3f3
Mason Katz <mjk@endobit.io>
2025-02-14 15:16:56 -0800

    checkpoint

--- rules/golang.mk
+++ rules/golang.mk
@@ -3,52 +3,54 @@ __golang_mk=1
 
 include $(BUILDER)/git.mk
 
+GO           = go
+GO_LINT      = golangci-lint
+GO_FMT       = go run golang.org/x/tools/cmd/goimports@latest
+GO_VULNCHECK = go run golang.org/x/vuln/cmd/govulncheck@latest
+
 ifneq (,$(GIT_VERSION))
 	GO_LDFLAGS = -ldflags "-X main.version=$(GIT_VERSION)"
 endif
-GO_BUILD = go build $(GO_LDFLAGS)
 
-go_langci_version = v1.62.0
-
-$(GOPATH)/bin:
-	mkdir -p $@
-
-$(GOPATH)/bin/golangci-lint: $(GOPATH)/bin
-	curl -sSfL \
-		https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | \
-		sh -s -- -b $(GOPATH)/bin $(go_langci_version)
+GO_BUILD = $(GO) build $(GO_LDFLAGS)
 
 format:: go-format
 lint:: go-lint go-vulncheck
 test:: go-test
 generate:: go-generate
 
-.PHONY: go-build go-format go-lint go-test go-vulncheck
+.PHONY: go-build go-format go-lint go-test go-vulncheck go-generate
 
 go-format:
 	@echo "$(c.INF)$@$(c.RST)"
-	@go run golang.org/x/tools/cmd/goimports@latest -w .
+	@$(GO_FMT) -w .
 
-go-lint: $(GOPATH)/bin/golangci-lint
+go-lint:
 	@echo "$(c.INF)$@$(c.RST)"
-	@$(GOPATH)/bin/golangci-lint version
-	@[ -e .golangci.yaml ] && $(GOPATH)/bin/golangci-lint run || true
-	@[ ! -e .golangci.yaml ] && $(GOPATH)/bin/golangci-lint run --config=$(BUILDER)/golangci.yaml || true
+	@$(GO_LINT) version
+ifneq ("$(wildcard .golangci.yaml)","")
+	@$(GO_LINT) run
+else
+	@$(GO_LINT) run --config=$(BUILDER)/golangci.yaml
+endif
 
 go-test:
 	@echo "$(c.INF)$@$(c.RST)"
-	go test -v -coverprofile=coverage.out ./...
+	@go test -v -coverprofile=coverage.out ./...
 
 go-vulncheck: 
 	@echo "$(c.INF)$@$(c.RST)"
-	go run golang.org/x/vuln/cmd/govulncheck@latest ./...
+	@$(GO_VULNCHECK) ./...
 
 go-generate:
 	@echo "$(c.INF)$@$(c.RST)"
-	go generate ./...
+	@go generate ./...
 
 dump::
-	@echo "GO_LDFLAGS   $(GO_LDFLAGS)"
+	@echo "GO           $(GO)"
 	@echo "GO_BUILD     $(GO_BUILD)"
+	@echo "GO_FMT       $(GO_FMT)"
+	@echo "GO_LDFLAGS   $(GO_LDFLAGS)"
+	@echo "GO_LINT      $(GO_LINT)"
 
 endif # __golang_mk
--- rules/rules.mk
+++ rules/rules.mk
@@ -3,15 +3,17 @@ __rules_mk=1
 
 .DEFAULT_GOAL = help
 
-.PHONY: generate build lint test clean nuke dump
-build:: ## build everything
-clean:: ## remove build artifacts
-dump:: ## rules debug info
-format:: ## run code formatters
+.PHONY: build clean dump format generate lint nuke test
+build::    ## build everything
+clean::    ## remove build artifacts
+dump::     ## rules debug info
+format::   ## run code formatters
 generate:: ## generate source code
-lint:: ## run linters
-nuke:: ## remove all build artifacts
-test:: ## run tests
+lint::     ## run linters
+nuke::     ## remove all build artifacts
+test::     ## run tests
+
+nuke:: clean
 
 ifneq ($(TERM),)
 include $(BUILDER)/color.mk
@@ -31,15 +33,16 @@ endif
 
 
 .PHONY: build-init
-builder-init:: ## (re)initialize build environment
+builder-init: ## (re)initialize build environment
 	go run github.com/endobit/builder@latest init
 
 help: ## print help
-	@egrep -oh '^[A-Za-z_\-]+:+ \#\#.*$$' $(MAKEFILE_LIST) | sort | \
-		awk 'BEGIN { FS = ":+ \#\#"} { printf "$(c.INF)%-16s$(c.RST) %s\n", $$1, $$2 }'
+	@egrep -oh '^[A-Za-z_\-]+:+ +\#\#.*$$' $(MAKEFILE_LIST) | sort | \
+		awk 'BEGIN { FS = ":+ +\#\#"} { printf "$(c.INF)%-16s$(c.RST) %s\n", $$1, $$2 }'
 
 -include $(BUILDER)/organization.mk
-COPYRIGHT     = "\(c\) $(shell date +%Y) $(ORGANIZATION)"
+
+COPYRIGHT = "\(c\) $(shell date +%Y) $(ORGANIZATION)"
 
 dump::
 	@echo "COPYRIGHT    $(COPYRIGHT)"
